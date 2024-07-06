4cb5ef2deffbb4908d573a45a11faa7bdf2875e4
Keadeish <you@example.com>
2024-07-06 13:48:25 +0100

    added Read Me

---
+++ calculator.ts
@@ -0,0 +1,20 @@
+function deadmanDropCalc(npcCombatLevel : number, npcName : string):string {
+    let text = ''
+    if (npcCombatLevel <= 0) {
+        text = "invalid NPC"
+    }
+    if (npcCombatLevel > 0 && npcCombatLevel <= 50) {
+        let calculation : number = (250 + ((Math.pow(50-npcCombatLevel,2)/5)))
+        text = `The chance of you hitting the drop table  is  1/${calculation} \n  you need to kill ${calculation} ${npcName}'s to get a drop`
+    }
+    if (npcCombatLevel > 50 && npcCombatLevel < 150) {
+        let calculation2 : number = (337.5-(1.75*npcCombatLevel))
+        text = `The chance of you hitting the drop table  is  1/${calculation2} \n  you need to kill ${calculation2} ${npcName}'s to get a drop`
+    }
+    if (npcCombatLevel >=150) {
+        let calculation3 : number = 75
+        text = `The chance of you hitting the drop table  is  1/${calculation3} \n  you need to kill ${75} ${npcName}'s to get a drop`
+    }
+    return text
+}
+console.log(deadmanDropCalc(82, 'Lesser Demon')) //replace 82 with the NPC's combat leve, and 'Lesser Demon' with the NPC name
