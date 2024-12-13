470525cb863e87808486484f9a46d88e0d1b51d9
keadeish <90222144+keadeish@users.noreply.github.com>
2024-12-13 15:13:24 +0000

    Update calculator.ts

--- calculator.ts
+++ calculator.ts
@@ -5,15 +5,15 @@ function deadmanDropCalc(npcCombatLevel : number, npcName : string):string {
     }
     if (npcCombatLevel > 0 && npcCombatLevel <= 50) {
         let calculation : number = (250 + ((Math.pow(50-npcCombatLevel,2)/5)))
-        text = `The chance of you hitting the drop table  is  1/${calculation} \n  you need to kill ${calculation} ${npcName}'s to get a drop`
+        text = `The chance of you hitting the drop table  is  1/${calculation} \n  you need to kill ${calculation} ${npcName}s to get a drop`
     }
     if (npcCombatLevel > 50 && npcCombatLevel < 150) {
         let calculation2 : number = (337.5-(1.75*npcCombatLevel))
-        text = `The chance of you hitting the drop table  is  1/${calculation2} \n  you need to kill ${calculation2} ${npcName}'s to get a drop`
+        text = `The chance of you hitting the drop table  is  1/${calculation2} \n  you need to kill ${calculation2} ${npcName}s to get a drop`
     }
     if (npcCombatLevel >=150) {
         let calculation3 : number = 75
-        text = `The chance of you hitting the drop table  is  1/${calculation3} \n  you need to kill ${75} ${npcName}'s to get a drop`
+        text = `The chance of you hitting the drop table  is  1/${calculation3} \n  you need to kill 75 ${npcName}s to get a drop`
     }
     return text
 }
